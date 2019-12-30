c2ec204fa8eb7460717dd8905f19f1661a0a6308
bonohub13 <ken.saito.0813@gmail.com>
2019-12-31 01:38:05 +0900

    default License

---
+++ device-drivers/led/.myled.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kensuke/.repos/my_gits/my_led/device-drivers/led/myled.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/kensuke/.repos/my_gits/my_led/device-drivers/led/myled.ko /home/kensuke/.repos/my_gits/my_led/device-drivers/led/myled.o /home/kensuke/.repos/my_gits/my_led/device-drivers/led/myled.mod.o ;  true
